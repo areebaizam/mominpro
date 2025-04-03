a0075203f80e909c51fb73804fd64ec2756b458b
Areeb <areebaizam@gmail.com>
2025-04-03 15:13:48 -0700

    Add Auth Service

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,10 +1,29 @@
 import { inject } from '@angular/core';
 import { CanMatchFn, Router } from '@angular/router';
 import { AuthStatusModel, HttpResponseModel } from '@core/models';
+import { AuthService } from '@core/services/auth.service';
 import { map } from 'rxjs/internal/operators/map';
 //Models
 // import { PagesURLConstants } from '@shared/models';
 
 export const AuthGuard: CanMatchFn = (route, segments) => {
-  return true;
+  const authService = inject(AuthService) as AuthService;
+  return authService.checkAuthStatus().pipe(
+    map((response: HttpResponseModel<AuthStatusModel>) => {
+      let isAuthenticated: boolean = response.status.isSuccess && response.next ? response.next.isAuthenticated : false;
+      let requiredRole = route.data ? route.data['role'] : 'Guest';
+
+      if (!isAuthenticated) {
+        console.log('Authentication Failed')
+        // router.navigateByUrl(`${PagesURLConstants.PAGES}/${PagesURLConstants.UNAUTHORIZED}`);
+        return false;
+      }
+      
+      if (!authService.hasRole(requiredRole)) {
+        console.log('Authorisation Failed')
+        // router.navigateByUrl(`${PagesURLConstants.PAGES}/${PagesURLConstants.FORBIDDEN}`);
+        return false;
+      }
+      return true;
+    }));
 };
---
+++ src/app/core/services/auth.service.ts
@@ -0,0 +1,47 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { inject, Injectable, signal } from '@angular/core';
+import { AuthStatusModel, HttpResponseModel, ApiURL } from '@core/models';
+import { catchError, Observable, of, tap, throwError } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private http = inject(HttpClient);
+  private userRoles: string[] = [];
+  isAuthenticated = signal<boolean>(false);
+
+  checkAuthStatus(): Observable<HttpResponseModel<AuthStatusModel>> {
+    return this.http.get<HttpResponseModel<AuthStatusModel>>(ApiURL.getAuthStatusUrl)
+      .pipe(
+        tap(response => {
+          let result = this.getResult(response);
+          this.isAuthenticated.set(result ? result.isAuthenticated : false);
+          this.userRoles = result ? result.claims.filter(claim => claim.type = 'http://schemas.microsoft.com/ws/2008/06/identity/claims/role').map(claim => claim.value) : [];
+        //   //TODO Move Claims to contants
+        }),
+        catchError(error => {  
+          // console.log('error hihihi',error);
+          //TODO Log ERROR
+          this.isAuthenticated.set(false); 
+          this.userRoles = [];
+          return this.handleHttpError<AuthStatusModel>(error);
+        })
+
+      );
+  }
+
+  hasRole(role: string): boolean {
+    return this.userRoles.includes(role);
+  }
+
+  //TODO move to utility
+  getResult<T>(resp: HttpResponseModel<T>): T | null {
+    return resp.status.isSuccess ? resp.next : null;
+  }
+
+  handleHttpError<T>(error: HttpErrorResponse): Observable<HttpResponseModel<T>> {
+    //TODO Log Error
+    return of({ errors: null, next: null, status: { isSuccess: false, message: 'Failure', statusCode: error.status, timeStamp: new Date().toISOString() } });
+  };
+}
