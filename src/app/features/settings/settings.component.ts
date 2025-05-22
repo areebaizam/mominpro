6d669b152b8dba066cfe7d47d72e75ff1d5c16f8
Areeb <areebaizam@gmail.com>
2025-05-22 14:56:12 -0700

    Update Settings to remove warning

--- src/app/features/settings/settings.component.ts
+++ src/app/features/settings/settings.component.ts
@@ -1,15 +1,15 @@
-import { Component, computed, DestroyRef, effect, inject, signal, ViewChild } from "@angular/core";
+import { Component } from "@angular/core";
 //Materials
 import { MatTabsModule } from "@angular/material/tabs";
 //Components
-import { BaseFormComponent } from "@shared/components";
+import { TabFormContainer } from "@shared/containers";
 //Utilities
 //Models
 import { generateTabs, SETTINGS_TAB_DEFINITIONS, SettingsTabKey, TabModel } from "@shared/models";
 //Constants
 const materialModules = [MatTabsModule];
-const components = [BaseFormComponent];
-const { tabForms: tabData, indexes: INDEXES } = generateTabs<SettingsTabKey, typeof SETTINGS_TAB_DEFINITIONS>(SETTINGS_TAB_DEFINITIONS);
+const components = [TabFormContainer];
+const { tabForms: tabFormsData, indexes: INDEXES } = generateTabs<SettingsTabKey, typeof SETTINGS_TAB_DEFINITIONS>(SETTINGS_TAB_DEFINITIONS);
 
 @Component({
     selector: "tap-settings",
@@ -18,31 +18,11 @@ const { tabForms: tabData, indexes: INDEXES } = generateTabs<SettingsTabKey, typ
     styleUrl: "./settings.component.scss",
 })
 export default class SettingsComponent {
-    //Child Components
-    @ViewChild('baseForm') baseForm!: BaseFormComponent;
-    //Dependency Injections
-    destroyRef = inject(DestroyRef);
-    // Variables
-    activeTabIndex = signal<number>(INDEXES.SALAH);
-    tabs: TabModel<SettingsTabKey>[] = tabData;
-
-    constructor() {
-        effect(() => {
-            //   if (this.activeTabIndex() === INDEXES.MOSQUE && this.authService.hasOrganisation()) {
-            //     this.fetchOrgData();
-            //   }
-        });
-    }
-
-    currentTabForms = computed(() => {
-        return this.tabs[this.activeTabIndex()].forms ?? [];
-    });
-
+    activeTabIndex = INDEXES.SALAH;
+    tabForms: TabModel<SettingsTabKey>[] = tabFormsData;
 
     submit(formValue: any) {
-        console.log("Form Value", formValue);
-        // if (this.activeTabIndex() == INDEXES.SALAH) {
-        //   this.addUpdateOrganisation(formValue);
-        // }
+        console.log('SettingsComponent', formValue);
     }
+
 }
